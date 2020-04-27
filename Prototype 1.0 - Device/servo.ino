bc7ad080981599a028260f4503f99476205abf79
이세민 <sky74zkr@gmail.com>
2020-04-27 11:42:33 +0900

    Prototype 1.1

--- Prototype 1.0 - Device/servo.ino
+++ Prototype 1.0 - Device/servo.ino
@@ -1,11 +1,13 @@
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-
+#include <SoftwareSerial.h>
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
-
+#define BT_RXD 3
+#define BT_TXD 2
+SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 // Depending on your servo make, the pulse width min and max may vary, you 
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
@@ -22,7 +24,7 @@ static int angleStep =10;
 void setup() {
   Serial.begin(9600);
   Serial.println("16 channel Servo test!");
-
+  bluetooth.begin(9600);
   board1.begin();
   board2.begin();  
   board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
@@ -48,9 +50,25 @@ int temp=0;
 //}
 //
 tempangle=0;
-board2.setPWM(temp, 0, angleToPulse(tempangle) );
-board1.setPWM(temp, 0, angleToPulse(tempangle) );
-  
+
+
+
+ if (bluetooth.available()) {       
+    Serial.write(bluetooth.read());  //블루투스측 내용을 시리얼모니터에 출력
+  }
+  if (Serial.available()) {         
+    bluetooth.write(Serial.read());  //시리얼 모니터 내용을 블루추스 측에 WRITE
+  }
+
+
+
+
+ if (bluetooth.available()) {       
+    Serial.write(bluetooth.read());  //블루투스측 내용을 시리얼모니터에 출력
+    board2.setPWM(temp, 0, angleToPulse(tempangle) );
+    board1.setPWM(temp, 0, angleToPulse(tempangle) );
+  }
+
 // robojax PCA9865 16 channel Servo control
   delay(1000);
 tempangle=180;
@@ -68,8 +86,8 @@ board1.setPWM(temp, 0, angleToPulse(tempangle) );
  */
 int angleToPulse(int ang){
    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-   Serial.print("Angle: ");Serial.print(ang);
-   Serial.print("pulse: ");Serial.println(pulse);
+//   Serial.print("Angle: ");Serial.print(ang);
+//   Serial.print("pulse: ");Serial.println(pulse);
    return pulse;
 }
 

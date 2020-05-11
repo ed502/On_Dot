2bf7aa1baac2f4bd9233eaaa784727efccfca0ac
이세민 <sky74zkr@gmail.com>
2020-05-12 00:37:56 +0900

    Update servo.ino

--- Prototype 1.0 - Device/servo.ino
+++ Prototype 1.0 - Device/servo.ino
@@ -1,24 +1,18 @@
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <SoftwareSerial.h>
-// called this way, it uses the default address 0x40
+//default address 0x40
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
 #define BT_RXD 3
 #define BT_TXD 2
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-// Watch video V1 to understand the two lines below: http://youtu.be/y8X9X10Tn1k
-#define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+// Watch video below: http://youtu.be/y8X9X10Tn1k
+#define SERVOMIN  100 // 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // 'maximum' pulse length count (out of 4096)
 
-
-int servoNumber = 0;
-static int angleStep =10;
+int servoNumber=0;
+static int angleStep=10;
 int angle=180;
 int angle2=180;
 int angle3=180;
@@ -29,6 +23,7 @@ char dotarray[10];
 int arraynum=0;
 int tempangle=0;
 int tempangle2=0;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("16 channel Servo test!");
@@ -37,22 +32,17 @@ void setup() {
   board2.begin();  
   board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   board2.setPWMFreq(60);
-  //yield();
 
-  for(int i = 0 ; i < 10 ; i++)
-  {
+  for(int i = 0 ; i < 10 ; i++) {
     dotarray[i]='1';
   }
-  
-  for(int i=0;i<10;i++)
-  {
+  for(int i=0;i<10;i++){
     Serial.println(dotarray[i]);
   }
 }
 
-// the code inside loop() has been updated by Robojax
+// robojax PCA9865 16 channel Servo control
 void loop() {
-//angle = angle + angleStep;
 //if (angle <=0 || angle >= 180) {
 //  angleStep = -angleStep;
 //}
@@ -64,115 +54,89 @@ void loop() {
 //            board1.setPWM(temp, 0, angleToPulse(angle) );
 //        }
 //}
-//
-
-
-
 // 보내는건 110010
  while (bluetooth.available()) {       
-//    Serial.write(bluetooth.read());  //블루투스측 내용을 시리얼모니터에 출력
    dotarray[arraynum]=bluetooth.read();
       Serial.println(dotarray[arraynum]);
    arraynum++;
-
    if(arraynum==6)
    {
     break;
    }
   }
-//  if (Serial.available()) {         
-//    bluetooth.write(Serial.read());  //시리얼 모니터 내용을 블루추스 측에 WRITE
-//  }
-
   arraynum = 0;
-
-
-
-// if (bluetooth.available()) {       
-//    Serial.write(bluetooth.read());  //블루투스측 내용을 시리얼모니터에 출력
-//    board2.setPWM(temp, 0, angleToPulse(tempangle) );
-//    board1.setPWM(temp, 0, angleToPulse(tempangle) );
-//  }
-
-// robojax PCA9865 16 channel Servo control
-//  delay(1000);
-
 //templeangle이 180이면 4 5 6 번 점자가 튀어나오고 90이면 들어감
 //templeangle2이 0 이면 1 2 3 번 점자가 튀어나오고 90이면 들어감
-if(dotarray[0]=='0')
-{
-  angle4=90;
-}
-else
-{
-  angle4=0;
-}
-if(dotarray[1]=='0')
-{
-   angle5=90;
-}
-else
-{
-   angle5=0;
-}
-if(dotarray[2]=='0')
-{
-   angle6=90;
-}
-else
-{
-   angle6=0;
-}
-if(dotarray[3]=='0')
-{
-   angle=95;
-}
-else
-{
-   angle=180;
-}
-if(dotarray[4]=='0')
-{
-   angle2=90;
-}
-else
-{
-   angle2=180;
-}
-if(dotarray[5]=='0')
-{
-   angle3=90;
-}
-else
-{
-   angle3=180;
-}
-
-board1.setPWM(0, 0, angleToPulse(angle) );
-board1.setPWM(1, 0, angleToPulse(angle2) );
-board1.setPWM(2, 0, angleToPulse(angle3) );
-board1.setPWM(3, 0, angleToPulse(angle4) );
-board1.setPWM(4, 0, angleToPulse(angle5) );
-board1.setPWM(5, 0, angleToPulse(angle6) );
+  if(dotarray[0]=='0')
+  {
+    angle4=90;
+  }
+  else
+  {
+    angle4=0;
+  }
+  if(dotarray[1]=='0')
+  {
+     angle5=90;
+  }
+  else
+  {
+     angle5=0;
+  }
+  if(dotarray[2]=='0')
+  {
+     angle6=90;
+  }
+  else
+  {
+     angle6=0;
+  }
+  if(dotarray[3]=='0')
+  {
+     angle=95;
+  }
+  else
+  {
+     angle=180;
+  }
+  if(dotarray[4]=='0')
+  {
+     angle2=90;
+  }
+  else
+  {
+     angle2=180;
+  }
+  if(dotarray[5]=='0')
+  {
+     angle3=90;
+  }
+  else
+  {
+     angle3=180;
+  }
+  board1.setPWM(0, 0, angleToPulse(angle) );
+  board1.setPWM(1, 0, angleToPulse(angle2) );
+  board1.setPWM(2, 0, angleToPulse(angle3) );
+  board1.setPWM(3, 0, angleToPulse(angle4) );
+  board1.setPWM(4, 0, angleToPulse(angle5) );
+  board1.setPWM(5, 0, angleToPulse(angle6) );
   delay(3000);
-// tempangle=90;
-// tempangle2=90;
-tempangle=180;
-tempangle2=0;
-board1.setPWM(0, 0, angleToPulse(tempangle) );
-board1.setPWM(1, 0, angleToPulse(tempangle) );
-board1.setPWM(2, 0, angleToPulse(tempangle) );
-board1.setPWM(3, 0, angleToPulse(tempangle2) );
-board1.setPWM(4, 0, angleToPulse(tempangle2) );
-board1.setPWM(5, 0, angleToPulse(tempangle2) );
+  tempangle=180;
+  tempangle2=0;
+  board1.setPWM(0, 0, angleToPulse(tempangle) );
+  board1.setPWM(1, 0, angleToPulse(tempangle) );
+  board1.setPWM(2, 0, angleToPulse(tempangle) );
+  board1.setPWM(3, 0, angleToPulse(tempangle2) );
+  board1.setPWM(4, 0, angleToPulse(tempangle2) );
+  board1.setPWM(5, 0, angleToPulse(tempangle2) );
   delay(3000);
 }
 
 /*
  * angleToPulse(int ang)
  * gets angle in degree and returns the pulse width
- * also prints the value on seial monitor
- * written by Ahmad Nejrabi for Robojax, Robojax.com
+ * written Robojax.com
  */
 int angleToPulse(int ang){
    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
@@ -211,18 +175,18 @@ int angleToPulse(int ang){
 //  Serial.print(" pulse: "); Serial.println(pulse);
 //  return pulse;
 //}
-////void setServoPulse(uint8_t n, double pulse) {
-////double pulselength;
-////pulselength = 1000000; // 1,000,000 us per second
-////pulselength /= 60; // 60 Hz
-////Serial.print(pulselength); Serial.println(" us per period");
-////pulselength /= 4096; // 12 bits of resolution
-////Serial.print(pulselength); Serial.println(" us per bit");
-////pulse *= 1000;
-////pulse /= pulselength;
-////Serial.println(pulse);
-////pwm.setPWM(n, 0, pulse);
-////}
+//void setServoPulse(uint8_t n, double pulse) {
+//double pulselength;
+//pulselength = 1000000; // 1,000,000 us per second
+//pulselength /= 60; // 60 Hz
+//Serial.print(pulselength); Serial.println(" us per period");
+//pulselength /= 4096; // 12 bits of resolution
+//Serial.print(pulselength); Serial.println(" us per bit");
+//pulse *= 1000;
+//pulse /= pulselength;
+//Serial.println(pulse);
+//pwm.setPWM(n, 0, pulse);
+//}
 //
 //
 //void loop() {
@@ -234,22 +198,22 @@ int angleToPulse(int ang){
 //board1.setPWM(15, 0, angleToPulse(angle) );
 //delay(100);
 //}
-////Serial.println(servonum);
-////for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-////pwm.setPWM(servonum, 0, pulselen);
-////}
-////
-////delay(10000);
-////
-////for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-////pwm.setPWM(servonum, 0, pulselen);
-////
-////}
-////
-////
-////delay(10000);
-////
-////servonum ++;
-////
-////if (servonum > 15) servonum = 0;
+//Serial.println(servonum);
+//for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+//pwm.setPWM(servonum, 0, pulselen);
+//}
+//
+//delay(10000);
+//
+//for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+//pwm.setPWM(servonum, 0, pulselen);
+//
+//}
+//
+//
+//delay(10000);
+//
+//servonum ++;
+//
+//if (servonum > 15) servonum = 0;
 //

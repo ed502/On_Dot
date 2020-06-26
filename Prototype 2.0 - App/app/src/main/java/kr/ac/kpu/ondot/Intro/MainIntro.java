fb1e1325736d2d463145eb544fb64ed997d4356a
mjhstar <56467872+mjhstar@users.noreply.github.com>
2020-06-26 18:24:13 +0900

    Prototype 3.0

--- Prototype 2.0 - App/app/src/main/java/kr/ac/kpu/ondot/Intro/MainIntro.java
+++ Prototype 2.0 - App/app/src/main/java/kr/ac/kpu/ondot/Intro/MainIntro.java
@@ -15,11 +15,6 @@ import kr.ac.kpu.ondot.CustomTouch.CustomTouchConnectListener;
 import kr.ac.kpu.ondot.CustomTouch.CustomTouchEvent;
 import kr.ac.kpu.ondot.CustomTouch.CustomTouchEventListener;
 import kr.ac.kpu.ondot.CustomTouch.FingerFunctionType;
-<<<<<<< HEAD
-=======
-import kr.ac.kpu.ondot.Main.BluetoothActivity;
-import kr.ac.kpu.ondot.Main.MainActivity;
->>>>>>> 2236b3e4739a8d469038c1654299af72a6caca5b
 import kr.ac.kpu.ondot.R;
 import kr.ac.kpu.ondot.Screen;
 
